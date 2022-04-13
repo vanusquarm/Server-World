c541a0941496a0e20d67e25b91cd15b028ae253e
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-04-13 18:21:11 +0000

    Establishing relationship between post and user
    Establishing relationship between post and user models

--- fast-api-python/models/post.py
+++ fast-api-python/models/post.py
@@ -11,3 +11,4 @@ class UserModel(database.Base):
   post_title = _sqlalchemy.Column(_sqlalchemy.String, index=True)
   post_description = _sqlalchemy.Column(_sqlalchemy.String, index=True)
   created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow())
+  user = _orm.relationship("User”, back_p0pulates="post")
