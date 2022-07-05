e1ef06fc30d4b8ee17b0817a77fe87ae01cd6584
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-07-05 23:23:50 +0000

    Create index.js

---
+++ graphql-mongoose/models/index.js
@@ -0,0 +1,13 @@
+//An index table for all models
+//As a good practice, all modules must import models from the models.js
+
+
+import Item from './Item.js'
+import Store from './Store.js'
+
+const models = {
+  Item,
+  Store
+}
+
+export default models;
