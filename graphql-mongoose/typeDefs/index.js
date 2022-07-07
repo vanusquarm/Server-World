8ceac69cc84817412174b2d953ef5e815e6de725
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-07-07 22:37:59 +0000

    Update index.js

--- graphql-mongoose/typeDefs/index.js
+++ graphql-mongoose/typeDefs/index.js
@@ -1,9 +1,11 @@
-import typedefs_store from "./store"
-import typedefs_item from "./item"
+import typedefs_store from "./store.js"
+import typedefs_item from "./item.js"
+import typedefs_cart from "./cart.js"
 
 const typedefs = {
   typedefs_store,
-  typedefs_item
+  typedefs_item,
+  typedefs_cart
 }
 
 export typedefs;
